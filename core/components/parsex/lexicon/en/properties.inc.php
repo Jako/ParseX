5cceeb343d5191ddb5716c8fa776929651dde481
Thomas Jakobi <thomas.jakobi@partout.info>
2024-12-03 15:52:18 +0100

    Fix lexicon typo

--- core/components/parsex/lexicon/en/properties.inc.php
+++ core/components/parsex/lexicon/en/properties.inc.php
@@ -5,10 +5,10 @@ $_lang['parsex.parsex.tpl'] = 'Template for one element';
 $_lang['parsex.parsex.wrapper'] = 'Wrapper template for all elements';
 $_lang['parsex.parsex.outputSeparator'] = 'Optional string to separate each tpl instance';
 $_lang['parsex.parsex.limit'] = 'Limits the number of elements returned (0 means no limit)';
-$_lang['parsex.parsex.filter'] = 'JSON encoded array of filter clauses (similar to XPDO where clauses)';
-$_lang['parsex.parsex.sortby'] = 'JSON encoded array of sortby clauses (at the moment only the first clause is regarded)';
+$_lang['parsex.parsex.filter'] = 'JSON-encoded array of filter clauses (similar to XPDO where clauses)';
+$_lang['parsex.parsex.sortby'] = 'JSON-encoded array of sortby clauses (at the moment only the first clause is regarded)';
 $_lang['parsex.parsex.debugmode'] = 'If true output some debug information after the normal output';
 $_lang['parsex.parsex.cacheData'] = 'Amount of time (in seconds) the XML feed will be cached';
 
-$_lang['parsex.parsexfilter.where'] = 'JSON encoded xPDO where clause to create the filter';
+$_lang['parsex.parsexfilter.where'] = 'JSON-encoded xPDO where clause to create the filter';
 $_lang['parsex.parsexfilter.params'] = 'Comma separated list of request parameters. Each request parameter could contain a query operator and query key prefix/suffix, appended colon separated, i.e.: &params=`requestparam:prefix:suffix`';
